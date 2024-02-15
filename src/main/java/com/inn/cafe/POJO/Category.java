baef5f025db6f85e3b953b06b8b6a28249b81e9f
gnichim <gnichim30@gmail.com>
2024-02-15 16:40:00 +0100

    all API of Category

---
+++ src/main/java/com/inn/cafe/POJO/Category.java
@@ -0,0 +1,29 @@
+package com.inn.cafe.POJO;
+
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@NamedQuery(name = "Category.getAllCategory", query = "select c from Category c")
+
+@Data // provides constructor, getter and setter
+@Entity
+@DynamicUpdate
+@DynamicInsert
+@Table(name = "category")
+public class Category implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id // To tell hibernate that this is the ID
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+}
---
+++ src/main/java/com/inn/cafe/serviceImpl/CategoryServiceImpl.java
@@ -0,0 +1,108 @@
+package com.inn.cafe.serviceImpl;
+
+import com.google.common.base.Strings;
+import com.inn.cafe.JWT.JwtFilter;
+import com.inn.cafe.POJO.Category;
+import com.inn.cafe.constants.CafeConstants;
+import com.inn.cafe.dao.CategoryDao;
+import com.inn.cafe.service.CategoryService;
+import com.inn.cafe.utils.CafeUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Slf4j // To print some logs in our console
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    @Autowired
+    CategoryDao categoryDao; // We Autowired the CategroyDao because we will use the object of it to store the data
+
+    @Autowired
+    JwtFilter jwtFilter;
+
+    @Override
+    public ResponseEntity<String> addNewCategory(Map<String, String> requestMap) {
+        try {
+            // Only the admin can add a category, so we need to check if the current user is an admin or not
+            if (jwtFilter.isAdmin()) {
+                if (validateCategoryMap(requestMap, false)) {
+                    categoryDao.save(getCategoryFromMap(requestMap, false));
+                    return CafeUtils.getResponseEntity("Category Added Successfully", HttpStatus.OK);
+                }
+            } else {
+                return CafeUtils.getResponseEntity(CafeConstants.UNAUTHORIZED_ACCESS, HttpStatus.UNAUTHORIZED);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private boolean validateCategoryMap(Map<String, String> requestMap, boolean validateId) {
+        if (requestMap.containsKey("name")) {
+            if (requestMap.containsKey("id") && validateId) {
+                return true;
+            } else if (!validateId) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Category getCategoryFromMap(Map<String, String> requestMap, Boolean isAdd) {
+        Category category = new Category();
+        if (isAdd) {
+            category.setId(Integer.parseInt(requestMap.get("id")));
+        }
+        category.setName(requestMap.get("name"));
+        return category;
+    }
+
+    @Override
+    public ResponseEntity<List<Category>> getAllCategory(String filterValue) {
+        try {
+            // We only need all these value which contains one or more product in it
+            if(!Strings.isNullOrEmpty(filterValue) && filterValue.equalsIgnoreCase("true")) {
+                log.info("Inside if");
+                return new ResponseEntity<List<Category>>(categoryDao.getAllCategory(), HttpStatus.OK);
+            }
+            return new ResponseEntity<>(categoryDao.findAll(), HttpStatus.OK);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return new ResponseEntity<List<Category>>(new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<String> updateCategory(Map<String, String> requestMap) {
+        try {
+            // Only the admin can update
+            if (jwtFilter.isAdmin()) {
+                if (validateCategoryMap(requestMap, true)) {
+                    Optional optional = categoryDao.findById(Integer.parseInt(requestMap.get("id")));
+                    // if optional is null this means that category does not exist
+                    if (!optional.isEmpty()) {
+                        categoryDao.save(getCategoryFromMap(requestMap, true));
+                        return CafeUtils.getResponseEntity("Category Updated Successfully", HttpStatus.OK);
+                    } else {
+                        return CafeUtils.getResponseEntity("Category id does not exist", HttpStatus.OK);
+                    }
+                }
+                return CafeUtils.getResponseEntity(CafeConstants.INVALID_DATA, HttpStatus.BAD_REQUEST);
+            } else {
+                return CafeUtils.getResponseEntity(CafeConstants.UNAUTHORIZED_ACCESS, HttpStatus.UNAUTHORIZED);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
