37eb3e80842ef44aa3fe5db44021cdc14b48f552
gnichim <gnichim30@gmail.com>
2024-02-29 12:13:11 +0100

    get product by ID

--- src/main/java/com/inn/cafe/dao/ProductDao.java
+++ src/main/java/com/inn/cafe/dao/ProductDao.java
@@ -19,4 +19,6 @@ public interface ProductDao extends JpaRepository<Product, Integer> {
 
     List<ProductWrapper> getProductByCategory(@Param("id") Integer id);
 
+    ProductWrapper getProductById(@Param("id") Integer id);
+
 }
