178e1f2dc2b1d4408c55b62f2222e58b318b82c0
gnichim <gnichim30@gmail.com>
2024-03-04 23:03:05 +0100

    getBills - getPdf - deleteBill

--- src/main/java/com/inn/cafe/rest/BillRest.java
+++ src/main/java/com/inn/cafe/rest/BillRest.java
@@ -1,10 +1,10 @@
 package com.inn.cafe.rest;
 
+import com.inn.cafe.POJO.Bill;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RequestMapping(path = "/bill")
@@ -13,5 +13,13 @@ public interface BillRest {
     @PostMapping(path = "/generateReport")
     ResponseEntity<String> generateReport(@RequestBody Map<String,Object> requestMap); // Map<String,Object> because we are expecting one list of json array and that will be of type object
 
+    @GetMapping(path = "/getBills")
+    ResponseEntity<List<Bill>> getBills();
+
+    @PostMapping(path = "getPdf")
+    ResponseEntity<byte[]> getPdf(@RequestBody Map<String, Object> requestMap);
+
+    @PostMapping(path = "/delete/{id}")
+    ResponseEntity<String> deleteBill(@PathVariable Integer id);
 
 }
